599aec8f9590a4f69e1c705e4d3310f86e14b14b
naitib <naitibhatt@gmail.com>
2020-05-29 18:12:09 -0400

    edit unzip

--- unzipFile.py
+++ unzipFile.py
@@ -12,17 +12,22 @@ URL = 'https://datasets.d2.mpi-inf.mpg.de/MobileHCI2018/MPIIMobileAttention.zip'
 
 download_url(URL, '../MPIIMobileAttention.zip')
 
+print ('url downloaded :) \n')
 
 # open and save the zip file onto computer
 url = urlopen(URL)
-output = open('./MPIIMobileAttention.zip', 'wb')    # note the flag:  "wb"        
+output = open('./MPIIMobileAttention.zip', 'wb')    # note the flag:  "wb"  
+print ('opened \n')      
 output.write(url.read())
+print ('written \n')  
 output.close()
+print ('closed \n')  
 
 # read the zip file as a pandas dataframe
 df = pd.read_pickle('MPIIMobileAttention.zip')    # zip files       
+print ('read a pkl \n')  
 
-df.to_pickle(".MPIIMobileAttention.pkl")
-
+df.to_pickle("./MPIIMobileAttention.pkl")
+print ('made a pkl \n')  
 # if keeping on disk the zip file is not wanted, then:
 # os.remove(zipName)   # remove the copy of the zipfile on disk
