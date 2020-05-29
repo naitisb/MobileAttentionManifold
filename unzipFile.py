a6e5382cf23186af3b517dcb242289691ed6f16c
naitib <naitibhatt@gmail.com>
2020-05-29 17:45:17 -0400

    add code for unzipping dataset

---
+++ unzipFile.py
@@ -0,0 +1,17 @@
+from zipfile import ZipFile
+from urllib.request import urlopen   
+import pandas as pd
+import os
+
+URL = 'https://datasets.d2.mpi-inf.mpg.de/MobileHCI2018/MPIIMobileAttention.zip'
+# open and save the zip file onto computer
+url = urlopen(URL)
+output = open('MPIIMobileAttention.zip', 'wb')    # note the flag:  "wb"        
+output.write(url.read())
+output.close()
+
+# read the zip file as a pandas dataframe
+df = pd.read_pickle('MPIIMobileAttention.zip')    # zip files       
+
+# if keeping on disk the zip file is not wanted, then:
+# os.remove(zipName)   # remove the copy of the zipfile on disk
